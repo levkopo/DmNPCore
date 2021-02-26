cf607c147326fd5e35ff52eaf93a692e2ac940d4
Domaman202 <vip.domaman@mail.ru>
2021-02-26 12:57:23 +0300

    setup

---
+++ src/commonMain/kotlin/ru/DmN/Project/core/DmNP.kt
@@ -0,0 +1,7 @@
+package ru.DmN.Project.core
+
+object DmNP {
+    const val Version = "1.0.0"
+    val Authors = arrayListOf("DomamaN202", "levkopo")
+    val Contacts = arrayListOf("https://github.com/Domaman202", "https://github.com/levkopo", "https://vk.com/levkopo")
+}
