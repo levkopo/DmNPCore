53aaa06f98454532237b69e4137be3d1b4714f05
Domaman202 <vip.domaman@mail.ru>
2021-02-27 15:14:12 +0300

    Update ObjectTest.kt

--- src/jvmTest/kotlin/ru/DmN/Project/test/ObjectTest.kt
+++ src/jvmTest/kotlin/ru/DmN/Project/test/ObjectTest.kt
@@ -5,13 +5,15 @@ import ru.DmN.Project.core.`object`.ObjType
 import ru.DmN.Project.core.`object`.api.IFMS
 import ru.DmN.Project.core.`object`.api.IObject
 import ru.DmN.Project.core.`object`.api.IVObject
+import ru.DmN.Project.core.`object`.impl.Object
+import ru.DmN.Project.core.`object`.impl.VObject
 import java.lang.Exception
 import kotlin.test.assertEquals
 
 class ObjectTest {
     @Test
     fun find1() {
-        for (i in 0 until 10000) {
+        for (counter in 0 until 10000) {
             val o1 = object : IObject, IFMS {
                 override val fields: ArrayList<IObject> = ArrayList()
                 override val methods: ArrayList<IVObject> = ArrayList()
@@ -19,28 +21,14 @@ class ObjectTest {
                 override val type: ObjType = ObjType.NULL
             }
 
-            val o2 = object : IVObject {
-                override val name: String = "TestObject2"
-                override val type: ObjType = ObjType.NULL
-                override var value: Any? = 12
-                    set(_) = throw Exception()
-            }
+            val o2 = VObject("TestObject2", ObjType.NULL, null)
 
             for (i in 0..1024) {
                 if (i == 256)
                     o1.methods.add(o2)
 
-                o1.fields.add(object : IObject {
-                    override val name: String = "O$i"
-                    override val type: ObjType = ObjType.NULL
-
-                })
-
-                o1.fields.add(object : IObject {
-                    override val name: String = "O${i + 1023}"
-                    override val type: ObjType = ObjType.NULL
-
-                })
+                o1.fields.add(Object("O$i", ObjType.NULL))
+                o1.fields.add(Object("O${i + 1023}", ObjType.NULL))
             }
 
             assertEquals(ru.DmN.Project.core.`object`.utils.find(o1, "TestObject2"), o2)
