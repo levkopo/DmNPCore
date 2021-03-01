996b534727cb25e37f3808ad8feef0576856a6b2
Domaman202 <vip.domaman@mail.ru>
2021-03-01 09:23:42 +0300

    Update IOCVirtualMachine.kt

--- src/commonMain/kotlin/ru/DmN/Project/core/vm/api/OC/IOCVirtualMachine.kt
+++ src/commonMain/kotlin/ru/DmN/Project/core/vm/api/OC/IOCVirtualMachine.kt
@@ -1,5 +1,6 @@
 package ru.DmN.Project.core.vm.api.OC
 
+import ru.DmN.Project.core.data.api.OC.IOCProvider
 import ru.DmN.Project.core.vm.api.base.IVirtualMachine
 
-interface IOCVirtualMachine<T> : IVirtualMachine<T>
+interface IOCVirtualMachine : IVirtualMachine<IOCProvider>
