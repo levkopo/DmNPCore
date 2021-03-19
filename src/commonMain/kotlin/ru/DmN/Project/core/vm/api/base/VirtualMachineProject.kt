8fb84316af69723bf639dcec0480fb167e28333d
Domaman202 <vip.domaman@mail.ru>
2021-03-19 15:24:58 +0700

    Create VirtualMachineProject.kt

---
+++ src/commonMain/kotlin/ru/DmN/Project/core/vm/api/base/VirtualMachineProject.kt
@@ -0,0 +1,23 @@
+package ru.DmN.Project.core.vm.api.base
+
+import ru.DmN.Project.core.obj.api.IEP
+import ru.DmN.Project.core.obj.api.IFMP
+import ru.DmN.Project.core.obj.api.IObject
+import ru.DmN.Project.core.obj.api.IVObject
+
+abstract class VirtualMachineProject {
+    abstract val name: String
+    abstract val version: String
+    abstract val authors: ArrayList<String>
+    abstract val license: String
+
+    abstract fun <T> toIObject(obj: T): IObject
+    abstract fun <T> toIVObject(obj: T): IVObject
+    abstract fun <T> toIEP(obj: T): IEP
+    abstract fun <T> toIFMP(obj: T): IFMP
+
+    abstract fun <T> ofIObject(obj: IObject): T
+    abstract fun <T> ofIVObject(obj: IVObject): T
+    abstract fun <T> ofIEP(obj: IEP): T
+    abstract fun <T> ofIFMP(obj: IFMP): T
+}
