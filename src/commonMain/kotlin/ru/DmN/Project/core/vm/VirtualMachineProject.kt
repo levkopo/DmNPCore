483fc6019567d36971386a70a05faa498f771b6f
Domaman202 <vip.domaman@mail.ru>
2021-03-30 15:08:20 +0700

    Update VirtualMachineProject.kt

--- src/commonMain/kotlin/ru/DmN/Project/core/vm/VirtualMachineProject.kt
+++ src/commonMain/kotlin/ru/DmN/Project/core/vm/VirtualMachineProject.kt
@@ -11,13 +11,13 @@ abstract class VirtualMachineProject {
     abstract val authors: ArrayList<String>
     abstract val license: String
 
-    abstract fun <T> toIObject(obj: T): IObject
-    abstract fun <T> toIVObject(obj: T): IVObject
-    abstract fun <T> toIEP(obj: T): IEP
-    abstract fun <T> toIFMP(obj: T): IDP
+    abstract fun <T> toIObject(obj: T?): IObject
+    abstract fun <T> toIVObject(obj: T?): IVObject
+    abstract fun <T> toIEP(obj: T?): IEP
+    abstract fun <T> toIFMP(obj: T?): IDP
 
-    abstract fun <T> ofIObject(obj: IObject): T
-    abstract fun <T> ofIVObject(obj: IVObject): T
-    abstract fun <T> ofIEP(obj: IEP): T
-    abstract fun <T> ofIFMP(obj: IDP): T
+    abstract fun <T> ofIObject(obj: IObject): T?
+    abstract fun <T> ofIVObject(obj: IVObject): T?
+    abstract fun <T> ofIEP(obj: IEP): T?
+    abstract fun <T> ofIFMP(obj: IDP): T?
 }
